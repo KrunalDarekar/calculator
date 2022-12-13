3f7a92aa678f301b63dee515a795489074a96575
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2022-12-13 14:07:24 +0530

    Link the buttons to the script and make the operations work

--- script.js
+++ script.js
@@ -30,3 +30,51 @@ function operate(a, operator ,b) {
         }
     }
 }
+
+const display = document.querySelector('.display');
+const digitButtons = document.querySelectorAll('.digit');
+const operatorButtons = document.querySelectorAll('.operator');
+const operateButton = document.querySelector('.equals');
+
+let arguments = [];
+let argumentNo = 0;
+let operators = [];
+let operatorNo = 0;
+
+digitButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent += button.textContent;
+    })
+})
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        arguments[argumentNo] = parseInt(display.textContent);
+        operators[operatorNo] = button.textContent;
+        display.textContent = "";
+        console.log(arguments[argumentNo]);
+        console.log(operators[operatorNo]);
+        argumentNo++;
+        operatorNo++;
+    })
+})
+
+operateButton.addEventListener('click', () => {
+    arguments[argumentNo] = parseInt(display.textContent);
+    console.log(arguments[argumentNo]);
+    console.table(arguments);
+    console.table(operators);
+    let prevArgument;
+    let newArgument;
+    for(let i = 0; i < operators.length; i++) {
+        if( i == 0 ) {
+            newArgument = operate(arguments[0],operators[i],arguments[1]);
+        } else {
+            prevArgument = newArgument;
+            console.log(prevArgument)
+            newArgument = operate(prevArgument,operators[i],arguments[i+1]);
+            console.log(newArgument);
+        }
+    }
+    display.textContent = `${newArgument}`;
+})
